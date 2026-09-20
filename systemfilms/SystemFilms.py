6e414e2edf83de29b9a530decb95fa6941e89837
Jeziel <jeziel.oliveira.paula@gmail.com>
2026-09-19 21:21:18 -0300

    Adicionando os comentários e separando por pastas os projetos

--- systemfilms/SystemFilms.py
+++ systemfilms/SystemFilms.py
@@ -1,26 +1,36 @@
+#Importando o SQLite3
 import sqlite3
 
+#Criando a classe Film
 class Film:
+    #Constructor da classe filme
     def __init__(self, name, year, genre, rating):
         self.name = name
         self.year = year
         self.genre = genre
         self.rating = rating
 
+    #Método para printar os dados do filme
     def __str__(self):
         return f"Título do filme: {self.name} \nAno de lançamento: {self.year} \nGênero(s) do filme: {self.genre} \nNota do filme: {self.rating}"
 
-
+#Criando a classe FilmsManager
 class FilmsManager:
+
+    #Método para printar os dados do filme de uma forma mais organizada
     def printFilms(self, id, film, year, genre, rating):
         print(f"ID: {id} \nTítulo do filme: {film} \nAno de lançamento: {year} \nGênero(s) do filme: {genre} \nNota do filme: {rating}\n {'-' * 30}")
 
-    def __init__(self, database):
+    #Constructor da classe FilmsManager
+    def __init__(self, database): 
         self.database = database
+        #Fazendo a conexão com o banco de dados
         self.connection = sqlite3.connect(self.database)
         self.cursor = self.connection.cursor()
+        #Criando a Tabela 
         self.createTable()
 
+    #Método para criar a Tabela no BD
     def createTable(self):
         self.cursor.execute("""
             CREATE TABLE IF NOT EXISTS films(
@@ -32,6 +42,7 @@ class FilmsManager:
             """)
         self.connection.commit()
 
+    #Método para adicionar um filme na tabela do banco de dados
     def addFilm(self, film):
         self.cursor.execute("""
             INSERT INTO films (name, year, genre, rating)
@@ -39,6 +50,7 @@ class FilmsManager:
         """, (film.name, film.year, film.genre, film.rating))
         self.connection.commit()
 
+    #Método para mostrar a lista de filmes que estão adicionados no BD
     def listFilms(self):
         self.cursor.execute("SELECT * FROM films")
         films = self.cursor.fetchall()
@@ -49,6 +61,7 @@ class FilmsManager:
         for film in films:
             self.printFilms(film[0], film[1], film[2], film[3], film[4])
 
+    #Método para procurar um filme pelo gênero
     def searchByGenre(self, genre):
         self.cursor.execute(
             """
@@ -63,6 +76,7 @@ class FilmsManager:
         for film in films:
              self.printFilms(film[0], film[1], film[2], film[3], film[4])
 
+    #Método para procurar um filme pelo ano de lançamento
     def searchByAge(self, year):
         self.cursor.execute(
             """                
@@ -78,6 +92,7 @@ class FilmsManager:
         for film in films:
                  self.printFilms(film[0], film[1], film[2], film[3], film[4])
 
+    #Método para procurar um filme pelo seu título
     def searchByName(self, title):
             self.cursor.execute(
                 """
@@ -92,6 +107,7 @@ class FilmsManager:
             for film in films:
                  self.printFilms(film[0], film[1], film[2], film[3], film[4])
 
+    #Método para deletar um filme pelo seu ID
     def deleteFilm(self, id):
         self.cursor.execute("""
             DELETE from films WHERE id = ?
@@ -99,7 +115,10 @@ class FilmsManager:
         self.connection.commit()
         print(f"O filme com o ID {id} foi deletado")
 
+#Instanciando o gerenciador de filmes 
 manager = FilmsManager("films.db")
+
+#Criando o menu, utilizando o match case
 while True:
     print("Bem vindo a nossa biblioteca de filmes")
     print("Escolha uma das opções abaixo:")
